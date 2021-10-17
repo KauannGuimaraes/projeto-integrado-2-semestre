2947e325591ff64bb4a925bcb0ee1065270e36db
Caio Belmont <caiohenriquebel@gmail.com>
2021-10-17 13:37:53 -0300

    Sistema View adicionado

---
+++ view/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+<div id="menu">
+    <ul>
+        <li><a href="products.php">Current Inventory</a></li>
+        <li><a href="incoming-purchases.php">Incoming Purchases</a></li>
+        <li><a href="outgoing-order.php">Outgoing Orders</a></li>
+        <li><a href="reports.php">Reports</a></li>
+        <li><a href="barcodes.php">Usuario</a></li>
+    </ul>
+</div>
+</body>
+</html>
