fa51ad4639b1c3327a0e022836938d5b2e199cdf
Caio Belmont <caiohenriquebel@gmail.com>
2021-10-17 16:50:17 -0300

    Alterções no Resultado do Estoque

--- view/estoque.php
+++ view/estoque.php
@@ -1,25 +1,32 @@
+
 <?php require '../Model/ProdutoDAO.php' ?>
 <html>
     <head>
     <link href="acess/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
 <body>
+
+
     <nav>
         <div id="menu">
             <ul>
-                <li><a href="../produto/add-produto.php">Cadastrar Produto</a></li>
-                <li><a href="../produto/decrement-produto.php">Descrementar Produto</a></li>
-                <li><a href="../produto/delete-produto.php">Remover Produto</a></li>
-                <li><a href="../produto/increment-produto.php">Incrementar Produto</a></li>
-                <li><a href="../barcodes.php">Usuario</a></li>
+                <li><a href="produto/add-produto.php">Cadastrar Produto</a></li>
+                <li><a href="produto/decrement-produto.php">Descrementar Produto</a></li>
+                <li><a href="produto/delete-produto.php">Remover Produto</a></li>
+                <li><a href="produto/increment-produto.php">Incrementar Produto</a></li>
+                <li><a href="estoque.php">Usuario</a></li>
             </ul>
         </div>
-    </nav>
+    </nav><br><br>
+    
+    <center>
     <table class="table">
   <thead>
     <tr>
       <th scope="col">Id</th>
       <th scope="col">Nome:</th>
+      <th scope="col">Descrição Produto:</th>
       <th scope="col">Quantidade:</th>
       <th scope="col">Preço:</th>
     </tr>
@@ -33,10 +40,12 @@
       <td><?php echo $result['idProduto'] ?></td>
       <td><?php echo $result['NomeProduto'] ?></td>
       <td><?php echo $result['DescricaoProduto'] ?></td>
+      <td><?php echo $result['QuantidadeProduto'] ?></td>
       <td><?php echo $result['PrecoProduto'] ?></td>
     </tr>
     <?php  } ?>
   </tbody>
 </table>
+</center>
 </body>
 </html>
