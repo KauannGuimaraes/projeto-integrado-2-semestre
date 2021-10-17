b5431ef7eb86072edef3c9253d6ceed6f8240eff
Kauann_guimaraes3 <kauannguimaraes3@gmail.com>
2021-10-17 13:52:45 -0300

    Telas produto

---
+++ view/decrement-produto.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="#">
+            <p>Decrementar produto:</p>
+            id do produto: <input type="number">
+            Quantidade: <input type="number">
+            <button type="submit">adicionar</button>
+        </form>
+    </body>
+</html>
---
+++ view/delete-produto.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="#">
+            <p>Remover produto:</p>
+            Id do produto: <input type="number">
+            <button type="submit">remover</button>
+        </form>
+    </body>
+</html>
