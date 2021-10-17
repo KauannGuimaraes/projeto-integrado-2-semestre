43695e2c21e7799a2d8e211f5bcbf5a4a6a6ea2f
Caio Belmont <caiohenriquebel@gmail.com>
2021-10-17 14:08:28 -0300

    bootstrap e pastas geradas

--- view/index.php
+++ view/index.php
@@ -1,16 +1,30 @@
 <html>
     <head>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 <body>
-<div id="menu">
-    <ul>
-        <li><a href="products.php">Current Inventory</a></li>
-        <li><a href="incoming-purchases.php">Incoming Purchases</a></li>
-        <li><a href="outgoing-order.php">Outgoing Orders</a></li>
-        <li><a href="reports.php">Reports</a></li>
-        <li><a href="barcodes.php">Usuario</a></li>
-    </ul>
-</div>
+    <nav>
+        <div id="menu">
+            <ul>
+                <li><a href="products.php">Gestão Estoque</a></li>
+                <li><a href="incoming-purchases.php">Gestão Pedidos</a></li>
+                <li><a href="outgoing-order.php">Outgoing Orders</a></li>
+                <li><a href="reports.php">Reports</a></li>
+                <li><a href="barcodes.php">Usuario</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section>
+        
+            <p>Products</p>
+        
+            <li>Nome</li>
+            <li>Nome</li>
+            <li>Nome</li>
+            <li>Nome</li>
+    </section>
+
+
 </body>
 </html>
---
+++ view/produto/add-produto.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        
+            <form action="#">
+                <p>Cadastrar produto:</p>
+                Nome do produto: <input type="text">
+                Quantidade: <input type="number">
+                <button type="submit">cadastrar</button>
+            </form>
+    </body>
+</html>
