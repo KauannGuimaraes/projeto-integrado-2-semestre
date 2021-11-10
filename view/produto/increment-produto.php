be969ec16fa5e59a817cf776e27e53dfa6c14e19
Kauann_guimaraes3 <kauannguimaraes3@gmail.com>
2021-11-10 17:15:12 -0300

    incremento de produto com opção fornecedor

--- view/produto/increment-produto.php
+++ view/produto/increment-produto.php
@@ -2,6 +2,7 @@
 <?php
     session_start();    
     require "../../Controller/Usuario.php";
+    require "../../Model/ClienteDAO.php"; 
     $login = new Usuario();
     $login->checkLogin();
 ?>
@@ -30,9 +31,37 @@
                     <label for="exampleInputPassword1" class="form-label">Quantidade</label>
                     <input type="number" class="form-control" name="quantidade">
                 </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Cliente:</label>
+                    <select id="productType" name="productType">
+                        <?php $clieDao = new ClienteDAO();
+                        $result = $clieDao->selecionaClientes();
+                        foreach($result as $result) {
+                            echo "<option value=".$result['idCliente'].">".$result['NomeCliente']."</option>";
+                        } ?>
+                    </select>
+                </div>
+                
                 <button type="submit" class="btn btn-warning">Incrementar</button>
             </form>
             </div>
+            
             </center>
+            <center>
+            <br><br><br>
+            <div class="card" style="width: 20rem;">
+            <form class="container-sm" action="../add-fornecedor-instance.php" method="POST">
+                <div class="mb-3">
+                    <label for="producttype" class="form-label">Nome do fornecedor:</label>
+                    <input type="text" class="form-control" name="nome">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Endereco do fornecedor:</label>
+                    <input type="text" class="form-control" name="endereco">
+                </div>
+                <button type="submit" class="btn btn-warning">cadastrar fornecedor</button>
+            </form>
+            </div>
+            </center>  
     </body>
 </html>
